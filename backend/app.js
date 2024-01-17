8c893bd0355cba91db684880c679a4ab581c66dc
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-01-18 00:23:21 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log(`User connected`);
+
+    socket.on('draw', (data) => {
+        socket.broadcast.emit('draw', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
