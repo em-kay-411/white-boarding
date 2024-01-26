8c00ab4f1af0a6286a05cef54c667ec8aa115feb
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-01-26 15:07:14 +0530

    Recording feature added but looks like screen recording

--- frontend/utils/record.js
+++ frontend/utils/record.js
@@ -1,25 +1,74 @@
+let mediaRecorder;
+let recordedChunks = [];
+
 function startOrStopRecording() {
-    if(!recording){
+    if (!recording) {
         recordButton.style.display = 'none';
         stopButton.style.display = 'block';
         recording = true;
         startRecording();
     }
-    else{
+    else {
         stopButton.style.display = 'none';
         recordButton.style.display = 'block';
-        recording  = false;
+        recording = false;
         stopRecording();
     }
 }
 
-function startRecording(){
-    resetTimer();
-    startTimer();
+async function startRecording() {
+    resetTimer();    
     console.log('Recording started');
+
+    let stream;
+
+    try {
+        stream = await navigator.mediaDevices.getDisplayMedia({
+            video: { mediaSource: 'screen' },
+            audio: true,
+        });
+    } catch {
+        stopButton.style.display = 'none';
+        recordButton.style.display = 'block';
+        recording = false;
+        stopRecording();
+        return;
+    }
+
+    startTimer();
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+            recordedChunks.push(event.data);
+        }
+    };
+
+    mediaRecorder.onstop = () => {
+        const blob = new Blob(recordedChunks, { type: 'video/mp4' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.style = 'display: none';
+        a.href = url;
+        a.download = `whiteboard-recording-${Date.now()}.mp4`;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+
+    mediaRecorder.start();
 }
 
 function stopRecording() {
+    try {
+        mediaRecorder.stop();
+    }
+    catch {
+        document.getElementById('timer').innerHTML = `Permission Denied`;
+        return;
+    }
+
     stopTimer();
     resetTimer();
     console.log('Recording stopped');
